e92e4dc13effed7d76f278c143cb86da8974243c
赵伟男 <zhaoweinann@126.com>
2017-10-23 22:05:28 +0800

    /Users/zhaoweinan

--- src/buysell/index.js
+++ src/buysell/index.js
@@ -84,8 +84,7 @@ const submit = () => {
       if (ajaxData.code !== 0) {
         Pop.alert(ajaxData.msg)
       } else {
-        // debugger;
-        redirect(`./my-order.html/?type=${urlData.search.type}&id=${ajaxData.data.orderId}`, '订单详情')
+        redirect(`./my-order.html?type=${urlData.search.type}&id=${ajaxData.data.orderId}`, '订单详情')
       }
     }
   })
